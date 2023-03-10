1cd2a8f1fe57fa9add8fc540527b27670c002058
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2023-03-10 16:17:56 +0200

    viewBinding and HomeFragment

--- app/src/main/java/com/example/senya/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/senya/ui/fragment/HomeFragment.kt
@@ -1,7 +1,32 @@
 package com.example.senya.ui.fragment
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.senya.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment() {
+class HomeFragment: BaseFragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = binding.recyclerView
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
