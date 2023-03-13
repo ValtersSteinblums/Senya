17d2804cdf7ae509a49cf9f050a4fd13001f4c21
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2023-03-13 15:35:20 +0200

    added location icon and menu

--- app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
@@ -1,10 +1,11 @@
 package com.example.senya.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.navigation.fragment.navArgs
+import com.example.senya.R
 import com.example.senya.data.Attraction
 import com.example.senya.databinding.FragmentAttractionDetailBinding
 import com.squareup.picasso.Picasso
@@ -16,6 +17,11 @@ class AttractionDetailFragment: BaseFragment() {
     private val safeArgs: AttractionDetailFragmentArgs by navArgs()
     private val attraction: Attraction by lazy {
         attractions.find { it.id == safeArgs.attractionId}!!
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +43,23 @@ class AttractionDetailFragment: BaseFragment() {
         binding.numbersOfFactsTextView.text = "${ attraction.facts.size } facts"
         binding.numbersOfFactsTextView.setOnClickListener {
             //todo
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_attraction_details, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menuItemlocation -> {
+                val uri = Uri.parse("geo:${attraction.location.latitude},${attraction.location.longitude}")
+                val mapIntent = Intent(Intent.ACTION_VIEW, uri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                startActivity(mapIntent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
