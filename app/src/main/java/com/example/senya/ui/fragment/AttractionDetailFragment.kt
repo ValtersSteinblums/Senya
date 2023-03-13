e5186806726a77ef30642056f4ffda1acb8ada3b
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2023-03-13 14:06:45 +0200

    added detailed description screen

--- app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.example.senya.data.Attraction
 import com.example.senya.databinding.FragmentAttractionDetailBinding
+import com.squareup.picasso.Picasso
 
-class AttractionDetailFragment: Fragment() {
+class AttractionDetailFragment: BaseFragment() {
     private var _binding: FragmentAttractionDetailBinding? = null
     private val binding get() = _binding!!
 
     private val safeArgs: AttractionDetailFragmentArgs by navArgs()
+    private val attraction: Attraction by lazy {
+        attractions.find { it.id == safeArgs.attractionId}!!
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,7 +29,15 @@ class AttractionDetailFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.button.text = safeArgs.attractionId
+        binding.titleTextView.text = attraction.title
+        Picasso.get().load(attraction.image_urls[0]).into(binding.headerImageView)
+        binding.monthsToVisitTextView.text = attraction.months_to_visit
+        binding.descriptionTextView.text = attraction.description
+
+        binding.numbersOfFactsTextView.text = "${ attraction.facts.size } facts"
+        binding.numbersOfFactsTextView.setOnClickListener {
+            //todo
+        }
     }
 
     override fun onDestroyView() {
